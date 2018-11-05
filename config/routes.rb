6a78a8f095f6051b21c6aae67be4136d2662f6cf
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-05 16:48:20 +0800

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'cocktails/new', to: 'cocktails#new'
+  post 'cocktails', to: 'cocktails#create'
+  get 'cocktails/index', to: 'cocktails#index'
+  # as: 'cocktails'
+  get 'cocktails/:id', to: 'cocktails#show', as: "cocktail"
+  # as: 'cocktail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
