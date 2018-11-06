7495c42a1e1c4d16beb5ca25d82a3cf0a47e5bf0
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-06 14:13:15 +0800

    updates

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,27 @@
 class DosesController < ApplicationController
   def new
+    @dose = Dose.new
+    cocktail_id = params[:cocktail_id]
+    @cocktail = Cocktail.find(cocktail_id)
+  end
+
+  def create
+    cocktail_id = params[:cocktail_id]
+    @cocktail = Cocktail.find(cocktail_id)
+
+    ingredient_id = params[:dose][:ingredient_id]
+    ingredient = Ingredient.find(ingredient_id)
+
+    description = params[:dose][:description]
+    dose = Dose.new(description: description)
+
+    dose.cocktail = @cocktail
+    dose.ingredient = ingredient
+
+    if dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      :new
+    end
   end
 end
